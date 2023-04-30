6759b4b2a6f876494a76c66ff499db035bd63639
Rustam T <sacred-foundry@yandex.ru>
2023-04-30 15:20:51 +0700

    update

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import re
 import csv
-from pprint import pprint
 with open("phonebook_raw.csv", encoding='utf-8') as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
-result_list,phone,name,organisation,position,email, = [],[],[],[],[],[]
+result_list, phone, name, organisation, position, email, = [], [], [], [], [], []
 buff = ""
 result_dict = {}
+
+
 def make_contacts_list():
-    for i in range(1,len(contacts_list)):
+    for i in range(1, len(contacts_list)):
         res = (re.findall(r'\d+\s*', ''.join(contacts_list[i])))
-        phone.append((''.join(res)).replace(' ',""))
+        phone.append((''.join(res)).replace(' ', ""))
         buff = ""
         for j in range(3):
             if len(contacts_list[i][j]) > 1:
@@ -20,42 +21,51 @@ def make_contacts_list():
         organisation.append(contacts_list[i][3])
         position.append(contacts_list[i][4])
         email.append(contacts_list[i][6])
+
+
 def make_phone_number():
     for i in range(len(phone)):
         if phone[i]:
-            phone[i] = '+7'+'('+phone[i][1:4]+')'+ phone[i][3:]    
-        if len (phone[i]) > 15:
-            phone[i] = phone[i][0:15] + ' доб.' + phone[i][15:]
-       
+            phone[i] = '+7'+'('+phone[i][1:4]+')' + phone[i][3:]    
+        if len(phone[i]) > 15:
+            phone[i] = phone[i][0:15] + ' доб.' + phone[i][15:]      
+
 
 def final_sort_list():
     for i in range(len(result_list)):
         try:
             if len((result_dict[" ".join(result_list[i][0].split()[0:2])].get('surname'))) == 0:
-                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'surname':" ".join(result_list[i][0].split()[2:3])})
+                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'surname': " ".join(result_list[i][0].split()[2:3])})
             if len((result_dict[" ".join(result_list[i][0].split()[0:2])].get('position'))) == 0:
-                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'position':result_list[i][2]})
+                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'position': result_list[i][2]})
             if len((result_dict[" ".join(result_list[i][0].split()[0:2])].get('email'))) == 0:
-                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'email':result_list[i][4]})
+                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'email': result_list[i][4]})
             if len((result_dict[" ".join(result_list[i][0].split()[0:2])].get('phone'))) == 0:
-                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'phone':result_list[i][3]})
+                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'phone': result_list[i][3]})
             if len((result_dict[" ".join(result_list[i][0].split()[0:2])].get('organization'))) == 0:
-                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'organization':result_list[i][1]})
+                result_dict[" ".join(result_list[i][0].split()[0:2])].update({'organization': result_list[i][1]})
 
-        except(KeyError):
+        except (KeyError):
             result_dict[" ".join(result_list[i][0].split()[0:2])] = {}
-            result_dict[" ".join(result_list[i][0].split()[0:2])]= ({'last_name':" ".join(result_list[i][0].split()[0:1]),'first_name':" ".join(result_list[i][0].split()[1:2]),'surname':" ".join(result_list[i][0].split()[2:3]),
-'organization':result_list[i][1],'position':result_list[i][2],'phone':result_list[i][3],'email':result_list[i][4]})
+            result_dict[" ".join(result_list[i][0].split()[0:2])] = (
+                {
+                    'last_name': " ".join(result_list[i][0].split()[0:1]),
+                    'first_name': " ".join(result_list[i][0].split()[1:2]),
+                    'surname': " ".join(result_list[i][0].split()[2:3]),
+                    'organization': result_list[i][1],
+                    'position': result_list[i][2],
+                    'phone': result_list[i][3],
+                    'email': result_list[i][4]
+                }
+                )
 
-# pprint((result_dict))
+
 if __name__ == '__main__':
     make_contacts_list()
     make_phone_number()
-    result_list = list(zip(name,organisation,position,phone,email))
+    result_list = list(zip(name, organisation, position, phone, email))
     final_sort_list()
 
-with open("phonebook.csv", "w",encoding='utf-8') as f:
-  datawriter = csv.writer(f, delimiter=',')
-  datawriter.writerows(result_dict.items())
-
-
+with open("phonebook.csv", "w", encoding='utf-8') as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(result_dict.items())
